8d7051d789cbab63c30769e9e90e83b37f45f3be
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-05-16 11:52:41 +0300

    added: reset password functionality

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
+import django.core.mail.backends.console
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +27,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = []
 
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as AuthenticationViews
+
 from home.views import home_page
 from users import views as auth_views
 
@@ -12,6 +14,27 @@ urlpatterns = [
     path('register/', auth_views.register_page, name='register'),
     path('login/', auth_views.login_page, name='login'),
     path('logout/', auth_views.logout_page, name='logout'),
+
+    path('password_change/done/', AuthenticationViews.PasswordChangeDoneView.as_view(
+        template_name='password_reset/password_change_done.html'),
+        name='password_change_done'),
+
+    path('password_change/', AuthenticationViews.PasswordChangeView.as_view(
+        template_name='password_reset/password_change.html'),
+        name='password_change'),
+
+    path('password_reset/done/', AuthenticationViews.PasswordResetCompleteView.as_view(
+        template_name='password_reset/password_reset_done.html'),
+        name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', AuthenticationViews.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+
+    path('password_reset/', AuthenticationViews.PasswordResetView.as_view(template_name='password_reset/password_reset_form.html'), name='password_reset'),
+
+    path('reset/done/', AuthenticationViews.PasswordResetCompleteView.as_view(
+        template_name='password_reset/password_reset_complete.html'),
+        name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
