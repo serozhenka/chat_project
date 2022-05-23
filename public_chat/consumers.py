01d414c0192195c22cb72fc7425b1c06027cd420
Shevchuk serozhenka <sergeykaoff@gmail.com>
2022-05-23 23:02:23 +0300

    added: display of chat messages

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -12,7 +12,9 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
         connection and is about to finish the WebSocket handshake.
         """
         print("public chat connect" + str(self.scope['user']))
+
         await self.accept()
+        await self.channel_layer.group_add("public_chatroom_1", self.channel_name)
 
     async def disconnect(self, code):
         """
@@ -22,7 +24,29 @@ class PublicChatConsumer(AsyncJsonWebsocketConsumer):
         await self.close(code=code)
 
     async def receive_json(self, content, **kwargs):
+        print(f"receive json: {content=}")
         command = content.get('command', None)
-        print(f"receive json: {command=}")
+        if command == "send":
+            await self.channel_layer.group_send(
+                "public_chatroom_1",
+                {
+                    "type": "chat.message",
+                    "image": self.scope['user'].image.url,
+                    "username": self.scope['user'].username,
+                    "user_id": self.scope['user'].id,
+                    "message": content['message'],
+                }
+            )
+
+    async def chat_message(self, event):
+        print(f"chat_message from: {event['user_id']}")
+        await self.send_json({
+            "image": event.get('image'),
+            "username": event.get('username'),
+            "user_id": event.get('user_id'),
+            "message": event.get('message').strip(),
+        })
+
+
 
 
